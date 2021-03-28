9ab9f9c1fd6b570727e557b93ae7d1b94cc6ea14
PriyankaRakshit <priyankarakshit259@gmail.com>
2021-03-28 23:48:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CardSettingsModel, SwimlaneSettingsModel } from '@syncfusion/ej2-angular-kanban';
+import { kanbanData } from './datasource';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-board-demo';
+  //https://ej2.syncfusion.com/angular/documentation/kanban/getting-started/
+
+  public data: Object[] = kanbanData;
+  public cardSettings: CardSettingsModel = {
+    contentField: 'Summary',
+    headerField: 'Id'
+  };
+  public swimlaneSettings: SwimlaneSettingsModel = { keyField: 'Assignee' };
 }
